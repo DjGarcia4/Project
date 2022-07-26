f7f9b949dd8e250ff78f9369c6f6319fca958332
Emerson Zapata <emerson.zapatarivas@gmail.com>
2022-07-26 14:53:55 -0600

    user_accounts_finiched

--- Usuarios/SQLUpdate_Usuarios.php
+++ Usuarios/SQLUpdate_Usuarios.php
@@ -4,18 +4,16 @@ if (isset($_POST['submit'])) {
   $id = $_POST['idUsuario'];
   $Usuario = $_POST['Usuario'];
   $NombreUsuario = $_POST['NombreUsuario'];
-  $Contrasenia = md5($_POST['Contrasenia']);
+  $Contrasenia = $_POST['Contrasenia'];
   $Estados_idEstado = $_POST['Estados_idEstado'];
   $Empleados_idEmpleados = $_POST['Empleado_idEmpleados'];
   $Empresas_idEmpresas = $_POST['Empresa'];
 
   $a = new database();
-  
-  $a->update('usuarios', 'idUsuario ='. $id.'', [
-    'Usuario' => $NombreUsuario, 'Contrasenia' => $Contrasenia,
-    'Empleados_idEmpleados' => $Empleados_idEmpleados, 'Empresas_idEmpresas' => $Empresas_idEmpresas,
-    'Estados_idEstado' => $Estados_idEstado
-  ]);
+
+  $a->ExecQuery('UPDATE `sistema_planilla`.`usuarios` 
+  SET `Usuario` = "'.$NombreUsuario.'", `Contrasenia` = md5("'.$Contrasenia.'"), `Empleados_idEmpleados` = "'.$Empleados_idEmpleados.'", `Empresas_idEmpresas` = "'.$Empresas_idEmpresas.'",
+  `Estados_idEstado` = "'.$Estados_idEstado.'" WHERE `idUsuario` = "'.$id.'";');
 
   if ($a == true) {
     header("location: TablaUsuarios.php?idUsuario=$Usuario&Empresas_idEmpresas=$Empresas_idEmpresas");
